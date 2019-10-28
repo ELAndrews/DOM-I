b3427041fb398119a3f14a20efe6275060d03ad2
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-10-28 15:34:17 +0000

    Small ammendments

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 const siteContent = {
   "nav": {
     "nav-item-1": "Services",
@@ -41,61 +42,16 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-onst siteContent = {
-  "nav": {
-    "nav-item-1": "Services",
-    "nav-item-2": "Product",
-    "nav-item-3": "Vision",
-    "nav-item-4": "Features",
-    "nav-item-5": "About",
-    "nav-item-6": "Contact",
-    "img-src": "img/logo.png"
-  },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
-    "img-src": "img/header-img.png"
-  },
-  "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-  },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
-  },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
-  },
-};
-
-// Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
-
 // ------- Header -------//
-const header = document.getElementsByTagName("header");
-
 const nav = document.querySelector("nav");
 const navA = Array.from(document.getElementsByTagName("a"));
 
-navA[0].textContent = "Services"
-navA[1].textContent = "Product"
-navA[2].textContent = "Vision"
-navA[3].textContent = "Features"
-navA[4].textContent = "About"
-navA[5].textContent = "Contact"
+navA[0].textContent = siteContent.nav["nav-item-1"];
+navA[1].textContent = siteContent.nav["nav-item-2"];
+navA[2].textContent = siteContent.nav["nav-item-4"];
+navA[3].textContent = siteContent.nav["nav-item-5"];
+navA[4].textContent = siteContent.nav["nav-item-6"];
+navA[5].textContent = siteContent.nav["nav-item-7"];
 
 navA.forEach(el => {
   el.style.color = "#14FA0C";
@@ -113,10 +69,9 @@ const headerImg = document.getElementById('logo-img');
 
 
 // ------- CTA area -------//
-const cta = document.getElementsByClassName("cta")
 
 const headText = document.getElementsByTagName("h1");
-headText[0].textContent = 'DOM Is Awesome';
+headText[0].textContent = 'DOM' + '\r\n' + 'Is Awesome';
 headText[0].style.fontSize = "5rem";
 
 const ctaBtn = document.getElementsByTagName("button")[0];
